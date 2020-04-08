42ee390a4e41e74192386cbee45441a358e4d2ef
José Domingo Álvarez <alvarezcaba9043@hotmail.com>
2020-04-08 22:48:56 +0200

    PRYSHIRT-001: Proyecto creado

---
+++ src/main/java/com/pryshirt/PryshirtApplication.java
@@ -0,0 +1,13 @@
+package com.pryshirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryshirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryshirtApplication.class, args);
+	}
+
+}
