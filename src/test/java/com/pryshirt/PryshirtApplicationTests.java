ac32c0c7dc129dbf331eb43b4c6910b271330e16
José Domingo Álvarez <alvarezcaba9043@hotmail.com>
2020-04-09 03:47:47 +0200

    PRYSHIRT-003: Pruebas unitarias creadas

--- src/test/java/com/pryshirt/PryshirtApplicationTests.java
+++ src/test/java/com/pryshirt/PryshirtApplicationTests.java
@@ -1,13 +1,50 @@
 package com.pryshirt;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-class PryshirtApplicationTests {
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pryshirt.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {PryshirtApplication.class, User.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class PryshirtApplicationTests {
+
+	@Autowired
+	private User user;
 
 	@Test
-	void contextLoads() {
+	public void testAssertTrue() {
+		List<User> users = new ArrayList<>();
+		users.add(user);
+		assertTrue(!users.isEmpty());
 	}
 
+	@Test
+	public void testAssertEquals() {
+		user.setName("Federico");
+		assertEquals("Federico", user.getName());
+	}
+
+	@Test
+	public void testAssertNotNull() {
+		user.setName("Federico");
+		assertNotNull(user.getName());
+	}
+
+	@Test
+	public void testAssertNull() {
+		user.setName(null);
+		assertNull(user.getName());
+	}
 }
